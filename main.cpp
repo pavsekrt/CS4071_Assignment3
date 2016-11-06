e09d1f23ee14a47d0a3bfc5fae849e569925ab40
Ryan Pavsek <pavsekrt@mail.uc.edu>
2016-11-06 01:42:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,33 +17,24 @@ int main(int argc, char* argv[])
   //Usage Test & setting V,E
   try{
     if ((argc < 5)||(*argv[argc-1] != '-')||(argc%2==0)||(atoi(argv[1]) != ((argc-3)/2))) {
-      throw;
+      throw(0);
     }
     for(size_t i = 0; i < atoi(argv[1]); i++){V.push_back(i);}
     for (size_t i = 2; i < (argc-1); i+=2) {
-      edge t = {atoi(argv[i]), atoi(argv[i+1])};
+      int x = atoi(argv[i]);
+      int y = atoi(argv[i+1]);
+      if (x > atoi(argv[1]) || x < 0 || y > atoi(argv[1]) || y < 0){
+        throw(0);
+      }
+      edge t = {x, y};
       E.push_back(t);
     }
   }catch (int e){
-    cerr << "Usage: " << argv[0] << " n i j (i j i j ...) -1" << endl;
-    cerr << "n = number of vertices, i j = sequence of pairs (integers between 0 and n)" << endl;
+    cerr << endl << "Usage: " << argv[0] << " n i j (i j i j ...) -1" << endl;
+    cerr << "n = number of vertices, i j = sequence of pairs (integers between 0 and n)" << endl << endl;
     return 1;
   }
-  //
-
-
-
-
-
-
-  // cout << "V: " << endl;
-  // for (size_t i = 0; i < V.size(); i++) {
-  //   cout << V.at(i) << ", ";
-  // }
-  // cout << endl << "E: " << endl;
-  // for (size_t i = 0; i < E.size(); i++) {
-  //   cout << "i = " << E.at(i).i << ", j = " << E.at(i).j << endl;
-  // }
+  //END Usage Test & setting V,E
 
 
 

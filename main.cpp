353d8a84c6f1fe028584a69dd82eb09dabb92c51
Ryan Pavsek <pavsekrt@mail.uc.edu>
2016-11-06 01:25:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,17 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  if ((argc < 5)||(*argv[argc-1] != '-')) {
+  //Usage Test
+  if ((argc < 5)||(*argv[argc-1] != '-')||(argc%2==0)) {
     cerr << "Usage: " << argv[0] << " n i j (i j i j ...) -1" << endl;
     cerr << "n = number of vertices, i j = sequence of pairs (integers between 0 and n)" << endl;
     return 1;
+  }
+  //
+
+  
+  for (size_t i = 1; i < argc-1; i++) {
+    cout << argv[argc-1] << endl;
   }
 
   return 0;
